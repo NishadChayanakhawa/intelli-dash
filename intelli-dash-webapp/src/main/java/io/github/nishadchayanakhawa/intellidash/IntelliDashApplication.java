3d04ad79f230032b3bfc7db3e26cdcf29bf8f204
nisha <nisha@host.docker.internal>
2023-07-09 08:25:41 +0530

    IN-6 completed spring boot application with user management.

---
+++ intelli-dash-webapp/src/main/java/io/github/nishadchayanakhawa/intellidash/IntelliDashApplication.java
@@ -0,0 +1,11 @@
+package io.github.nishadchayanakhawa.intellidash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliDashApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliDashApplication.class, args);
+	}
+}
